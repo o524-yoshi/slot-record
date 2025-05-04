bc0ea4075b1b4e0f07224ef2c17104a395ae62a5
unknown <yoshio_ito@r.recruit.co.jp>
2025-05-05 02:04:03 +0900

    fix: specify type for useSearchParams

--- app/hokuto-question2/page.tsx
+++ app/hokuto-question2/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { supabase } from '../../lib/supabase'
 
 export default function Question2() {
-  const searchParams = useSearchParams({ suspense: false })
+  const searchParams: URLSearchParams = useSearchParams()
   const userId = searchParams.get('user') || ''
   const machineName = searchParams.get('color') || ''
   const [answer, setAnswer] = useState('')
